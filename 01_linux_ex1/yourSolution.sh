1b32ea47223a29a0e4b8ccd4dab71c25311ff57c
polonskyilya <ilya@polonsky.net>
2022-06-15 20:15:27 +0300

    Correction to yourSolution.sh

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
 tar -xvf secretGenerator.tar.gz
-cat src/generateSecret.sh
 rm -r src/maliciousFiles
 rm src/important.link
 mkdir src/secretDir
