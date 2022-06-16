3841d6d49027e5e5401f9f3d49731d6cba264b30
mustafas01 <bassh967@gmail.com>
2022-06-16 23:35:44 +0300

    Linux ex1 solution

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -6,4 +6,4 @@ rm -r ./maliciousFiles
 rm important.link
 touck 4.txt
 ln -s ./4.txt important.link
-./yourSolution.sh   ##cat ./CONTENT_TO_HASH | xargs | md5sum > secretDir/.secret
+./yourSolution.sh ##cat ./CONTENT_TO_HASH | xargs | md5sum > secretDir/.secret
