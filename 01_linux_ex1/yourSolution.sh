cbedbde55d32b27d24fa7e32eaa600bb747aa656
mustafas01 <bassh967@gmail.com>
2022-06-16 23:33:57 +0300

    Linux ex1 solution

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,1 +1,9 @@
-cat ./CONTENT_TO_HASH | xargs | md5sum > secretDir/.secret
+/bin/bash
+
+mkdir secretDir/.secret
+chmod 600 ./secretDir/.secret
+rm -r ./maliciousFiles
+rm important.link
+touck 4.txt
+ln -s ./4.txt important.link
+./yourSolution.sh   ##cat ./CONTENT_TO_HASH | xargs | md5sum > secretDir/.secret
