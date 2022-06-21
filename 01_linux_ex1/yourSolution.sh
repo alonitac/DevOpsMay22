8325d69aef74d7218e649620d5c59f9e95d5f098
Sharon.Levi <sharont55l@gmail.com>
2022-06-21 10:32:17 +0300

    Linux EX1 Solution edited and fixed

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -18,12 +18,13 @@ if [ -d "src/secretDir" ]; then
   echo "The directory secretDir is already exists" && sleep 2
 else
   echo "Creating dir SecretDir in src dir" && sleep 2
-  mkdir secretDir
+  mkdir secretDir/ -v
 fi
+
 # Checks if maliciousFiles dir is exists and remove it else show message.
 if [ -d "glemaliciousFiles" ]; then
   echo "Deleting MaliciousFiles dir from the src dir" && sleep 2
-  sudo rm -rf src/maliciousFiles
+  rm -rf src/maliciousFiles -v
 else
        echo " malicius files is already deleted " && sleep 2
 fi
@@ -31,22 +32,25 @@ fi
 # Checks if .secret file in the secretDir if not create the .secret file in this dir.
 if [ ! -f "secretDir/.secret" ]; then
         echo -e "Creating .secret file in the src/secretDir dir " && sleep 2
-  cd secretDir/
-  ls -a
-  sudo touch .secret
+  touch secretDir/.secret
 fi
 
 # Checks if the .secret file permission is not 600 and add R and W to the other users
 OCTAL_PERMISSIONS=$(stat -c "%a" src/secretDir/.secret)
 if [ "$OCTAL_PERMISSIONS" != "600" ]; then
   echo "changing file .secret permissions to read and write only for the other users" && sleep 2
-  sudo chmod 006 src/secretDir/.secret
+  sudo chmod 600 src/secretDir/.secret
 fi
 
-# practice file linking understanding
+# Creating file someFileIsLinkingToMe.BeAware.
+echo "New file creation : someFileIsLinkingToMe.BeAware"
+touch someFileIsLinkingToMe.BeAware
+
+# unlink and link important file to the file above
 if [ -L 'src/important.link' ] && [ ! -e 'src/important.link' ]; then
-  echo "Remove the linking from important file" && sleep 2
+  echo "Remove the linking from important file a redirect for the someFileIsLinkingToMe.BeAware file" && sleep 2
   sudo unlink src/important.link
+  ln -s someFileIsLinkingToMe.BeAware important.link -v
 fi
 
 sudo cat $HOME/src/CONTENT_TO_HASH | xargs | md5sum > src/secretDir/.secret && echo "Done! Your secret was stored in secretDir/.secret"
