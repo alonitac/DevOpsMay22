0a5aba3beba3cf08b58d7488b092367606531854
Sharon.Levi <sharont55l@gmail.com>
2022-06-21 11:33:13 +0300

    Linux EX1 Solution edited and fixed

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -11,26 +11,28 @@ echo "
 ║  ├┬┘├┤ ├─┤ │ ├┤  ││  ├┴┐└┬┘  ╚═╗├─┤├─┤├┬┘│ ││││  ║  ├┤ └┐┌┘│
 ╚═╝┴└─└─┘┴ ┴ ┴ └─┘─┴┘  └─┘ ┴   ╚═╝┴ ┴┴ ┴┴└─└─┘┘└┘  ╩═╝└─┘ └┘ ┴
 "
-read -p "Press enter to start the program"
-
 #Checks if secretDir is already exists else create a new dir.
-  echo "Creating dir SecretDir in src dir" && sleep 2
+  echo "Creating dir SecretDir in src directory" && sleep 2
   mkdir secretDir
 
 # Checks if maliciousFiles dir is exists and remove it else show message.
-  echo "Deleting MaliciousFiles dir from the src dir" && sleep 2
+  echo "Deleting MaliciousFiles directory from the src directory" && sleep 2
   rm -rf maliciousFiles
 
+#Verify changes
 ls -la
+
 # Creating .secret file in secretDir
   echo -e "Creating .secret file in the src/secretDir dir " && sleep 2
-  sudo touch secretDir/.secret
+  cd secretDir
+  sudo touch .secret
 
 # Checks if the .secret file permission is not 600 and add R and W to the other users
-OCTAL_PERMISSIONS=$(stat -c "%a" src/secretDir/.secret)
+OCTAL_PERMISSIONS=$(stat -c "%a" .secret)
 if [ "$OCTAL_PERMISSIONS" != "600" ]; then
-  echo "changing file .secret permissions to read and write only for the other users" && sleep 2
-  sudo chmod 600 secretDir/.secret
+  echo "changing file .secret permissions to read and write" && sleep 2
+  sudo chmod 600 .secret
+  cd ..
 fi
 
 # Creating file someFileIsLinkingToMe.BeAware.
@@ -38,9 +40,9 @@ echo "New file creation : someFileIsLinkingToMe.BeAware"
 touch someFileIsLinkingToMe.BeAware
 
 # unlink and link important file to the file above
-if [ -L 'src/important.link' ] && [ ! -e 'src/important.link' ]; then
+if [ -L 'important.link' ] && [ ! -e 'important.link' ]; then
   echo "Remove the linking from important file a redirect for the someFileIsLinkingToMe.BeAware file" && sleep 2
-  sudo unlink src/important.link
+  sudo unlink important.link
   ln -s someFileIsLinkingToMe.BeAware important.link -v
 fi
 
