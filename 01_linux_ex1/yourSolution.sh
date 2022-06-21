f0266e1091ef77f7a515122ab26c70b037b39bb4
Sharon.Levi <sharont55l@gmail.com>
2022-06-21 09:27:37 +0300

    Linux EX1 Solution edited and fixed

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -44,7 +44,7 @@ fi
 # Checks if maliciousFiles dir is exists and remove it else show message.
 if [ -d "src/maliciousFiles" ]; then
   echo "Deleting MaliciousFiles dir from the src dir" && sleep 2
-  sudo rm -rf src/maliciousFiles
+  sudo rm -rf $HOME/src/maliciousFiles
 else
        echo " malicius files is already deleted " && sleep 2
 fi
@@ -52,7 +52,7 @@ fi
 # Checks if .secret file in the secretDir if not create the .secret file in this dir.
 if [ ! -f "src/secretDir/.secret" ]; then
         echo -e "Creating .secret file in the src/secretDir dir " && sleep 2
-  sudo touch src/secretDir/.secret
+  sudo touch $HOME/src/secretDir/.secret
 fi
 
 # Checks if the .secret file permission is not 600 and add R and W to the other users
