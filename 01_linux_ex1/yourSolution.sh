477242bbfd767a7b6875aa2eb1792d180ab034e6
ofex <ofeky.hisky@gmail.com>
2022-06-21 20:30:27 +0300

    my 2nd commit

---
+++ 01_linux_ex1/yourSolution.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+srcPath="$PWD/src"
+
+wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
+tar -xvzf secretGenerator.tar.gz
+
+cd $srcPath
+mkdir secretDir
+rm -rf maliciousFiles
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+rm important.link
+ln -s "$srcPath/secretDir/.secret" important.link
+chmod +x generateSecret.sh
+./generateSecret.sh
+
