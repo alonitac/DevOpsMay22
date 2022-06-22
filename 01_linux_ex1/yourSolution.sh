a233b51fc9cb39c86b7d475c9781b49cd46d47bc
naor <naorbenarii@gmail.com>
2022-06-23 01:27:49 +0300

    update

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -5,11 +5,17 @@ ls -l # checks the name of the file that i got
 tar xvf secretGenerator.tar.gz
 #goes to check if any  maliciousFiles are existing
 rm -r  src/maliciousFiles
+rm  src/important.link
 - # rm -r command can remove a dir
 mkdir src/secretDir
 #now i need to give the directory permissions
 chmod 764 src/secretDir
 umask 066 # in order to give the files that i create permissions
 cd src/secretDir
+touch .secret #creates a fuke
+cd ~
+cd home/
+chmod 777 generateSecret.sh
+sudo ./generateSecret.sh
 
  # now needed to create a file  ./secret
