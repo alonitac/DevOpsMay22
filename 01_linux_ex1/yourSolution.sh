7d1075687e51ffd4c2095a0c5cdd03a5f95953ab
ofex <ofeky.hisky@gmail.com>
2022-06-23 17:54:35 +0300

    my 4th commit

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+# Ofek Yanin solution ;-)
 srcPath="$PWD/src"
 
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
 tar -xvzf secretGenerator.tar.gz
-
 cd $srcPath
 mkdir secretDir
 rm -rf maliciousFiles
@@ -13,4 +13,5 @@ rm important.link
 ln -s "$srcPath/secretDir/.secret" important.link
 chmod +x generateSecret.sh
 ./generateSecret.sh
+
 
