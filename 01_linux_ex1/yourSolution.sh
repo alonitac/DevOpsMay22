b8b5222ef01ad6696fa5d1668f5ad874e628dd57
RoyOps <roey.dev@gmail.com>
2022-06-26 12:09:03 +0300

    Secret Kernel System Calls Broken Symlink Binary Numbers - wip

---
+++ 01_linux_ex1/yourSolution.sh
@@ -0,0 +1,11 @@
+# /bin/bash generateSecret.sh
+mkdir secretDir
+rm -r maliciousFiles/
+touch secretDir/.secret
+cd secretDir
+chmod 600 .secret
+cd ..
+find -xtype l -delete
+/bin/bash generateSecret.sh
+
+
