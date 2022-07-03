ed98b2678d493ce9bb7c4b39cc89ad526e0a2955
matan <matankaufman1@gmail.com>
2022-07-03 18:08:59 +0300

    Matan Kaufman solution Ex01

---
+++ 01_linux_ex1/yourSolution.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#! mkdir secretDir
+
+#! rm -r maliciousFiles/
+
+#!cd secretDir/
+
+#! touch .secret
+
+#!chmod 600 .secret
+
+#! cd ..
+
+#! find -xtype l -delete
+
+#! /bin/bash generateSecret.sh
