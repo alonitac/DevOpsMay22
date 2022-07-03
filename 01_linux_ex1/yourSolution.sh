03ef12e817b69be94ed11011e1c8ff627070ea89
NRO1 <nrdevac1@gmail.com>
2022-07-03 12:12:15 +0300

    Nati Roth solution to EX1 - devops- may22

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,11 +1,9 @@
-tar -xf secretGenerator.tar.gz
-cd src
 chmod 777 generateSecret.sh
 mkdir secretDir
-rm -r /maliciousFiles
 cd secretDir
 touch .secret
 chmod 600 .secret
 cd ..
+rm -r maliciousFiles
 rm important.link
-./generateSecret.sh
+/bin/bash generateSecret.sh
