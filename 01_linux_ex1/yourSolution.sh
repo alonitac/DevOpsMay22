bdcdd6c2fb4b59d585d0a9751046b8619dbb65a1
ofirg <ofir.goaris3@gmail.com>
2022-07-04 18:52:28 +0300

    fixing test #1

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,8 +1,12 @@
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
 tar -xvf secretGenerator.tar.gz
+cd src/
+rm -r important.link && rm -r maliciousFiles/
 mkdir secretDir
+cd secretDir
 touch .secret
-chmod 777 src/secretDir/.secret
-rm -r maliciousFiles/
-rm important.link
+chmod 777 .secret
+cd ..
 /bin/bash generateSecret.sh
+cd secretDir/
+cat .secret
