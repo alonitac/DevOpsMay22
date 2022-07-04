dba72b86f9bb0cedab7c54d923745a65924f3643
aleksei1988 <checkpcreview@gmail.com>
2022-07-04 12:21:01 +0300

    Linux ex1 solution

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
 tar -xvf secretGenerator.tar.gz
+mkdir src/secretDir
+touch src/secretDir/.secret
+chmod 600 src/secretDir/.secret
 rm -r src/maliciousFiles/
 rm src/important.link
-mkdir src/secretDir
-chmod 700 src/secretDir
-cd src/secretDir
-touch .secret
-chmod 600 .secret
-cd ..
-chmod 700 generateSecret.sh
+cd src/
 /bin/bash generateSecret.sh
