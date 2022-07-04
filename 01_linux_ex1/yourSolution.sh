6f5a7a14960336618bb6f7f04fd5a4ebb6e9982a
Aleksei <107418768+aleksei1988@users.noreply.github.com>
2022-07-04 12:05:07 +0300

    Update yourSolution.sh

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -2,10 +2,8 @@
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
 tar -xvf secretGenerator.tar.gz
 mkdir src/secretDir
-cd src/secretDir
-touch .secret
-chmod 600 .secret
+touch src/secretDir/.secret
+chmod 600 src/secretDir/.secret
 rm -r src/maliciousFiles/
 rm src/important.link
-cd ..
 /bin/bash generateSecret.sh
