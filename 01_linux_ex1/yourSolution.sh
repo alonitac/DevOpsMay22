c329aa1c7a95c653bf9524028952f7ee9ec7885d
aleksei1988 <checkpcreview@gmail.com>
2022-07-04 18:23:52 +0300

    test

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
-tar -xvf secretGenerator.tar.gz
 mkdir src/secretDir
 touch src/secretDir/.secret
 chmod 600 src/secretDir/.secret
-rm -r src/maliciousFiles/
-rm src/important.link
 cd src/
+rm -r maliciousFiles/
+rm important.link
 /bin/bash generateSecret.sh
