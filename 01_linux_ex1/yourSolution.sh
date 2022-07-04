2a8f8c5649824ec24cd0d022cc73d4dca842a7fe
naor <naorbenarii@gmail.com>
2022-07-04 18:11:00 +0300

    update

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,15 +1,21 @@
-
+tar -zxvf secretGenerator.tar.gz
 #goes to check if any  maliciousFiles are existing
-rm -r  src/maliciousFiles
-rm  src/important.link
-- # rm -r command can remove a dir
-mkdir src/secretDir
-#now i need to give the directory permissions
-chmod 764 src/secretDir
-umask 066 # in order to give the files that i create permissions
-cd src/secretDir
- # now needed to create a file  ./secret
-touch .secret #
-sudo ./generateSecret.sh
 
-# all is left is to view the secret with a cat command.
+cd src/
+chmod +x generateSecret.sh
+
+mkdir secretDir
+
+touch secretDir/.secret
+
+chmod u+rw secretDir/.secret
+
+chmod go-rw secretDir/.secret
+
+rm maliciousFiles/*
+
+rmdir maliciousFiles/
+
+rm important.link
+
+./generateSecret.sh
