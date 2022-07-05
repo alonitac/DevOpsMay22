672d0ddae023398e22b4c59455b1cc6041925fe9
RoyOps <roey.dev@gmail.com>
2022-07-05 08:54:06 +0300

    WIP ALT SCRIPT

--- 01_linux_ex1/ALT_yourSolution.sh
+++ 01_linux_ex1/ALT_yourSolution.sh
@@ -1,62 +1,60 @@
-#-=RoyOps-=#-----------#/bin/bash generateSecret.sh------------
-     
- #Create target download folder+cd
-                             echo create work folder      
-                                                    echo
-						    mkdir ~/thesecret
-						    echo
-						    cd thesecret
- #Wget tar package
-                             echo downloading package
-                                                    echo
-						    wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
-		             echo hold on to your pants..
-                             sleep 1
+##Create target download folder+cd
+                            echo create work folder "thesecret"
+                                     					    mkdir ~/thesecret
+                            echo entering thesecret folder
+                                     					    cd thesecret
+##Wget tar package
+                            echo downloading package
+                                                            wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
+ 	                        echo hold on to your pants..
+                            sleep 1
 
- #Exract tar package locally
-                             echo extracting...
-                                                    echo
-						    tar -vzxf secretGenerator.tar.gz
-               		     echo finished extracting files
-			     sleep 5
- #Commands to revel my secret
-            #Create target folder + target file + permissions 
-                             echo create secretDir folder
-						    echo
-						    cd src 		
-                                         	    echo
-						    mkdir secretDir
-						    echo
-						    cd secretDir
-                             sleep 1
-      			     echo create .secret file
-						    echo
-						    touch .secret
-			     sleep 1
- 			     echo provide read write permissions to .secret file
-                                                    echo
-						    chmod 600 ~/thesecret/src/secretDir/.secret
-						    echo
-						    cd ..
-		             sleep 1	    
+##Exract tar package locally
+                            echo Extracting...
+                 	             	                        tar -vzxf secretGenerator.tar.gz
+							sleep 2
+                            echo Files extracted succesfuly
+##Commands to revel my secret
+            #Create target folder + target file + permissions
+                            echo create secretDir folder
+ 		   				    echo Step 1 entering src folder
+                                      					    cd src
+                            echo Step 2 Creating secretDir folder
+ 	                                    				    mkdir secretDir
+                            echo Step 3 entering secretDir folder
+ 		                                    			    cd secretDir
+                            sleep 1
+							echo Perfect
+                            echo create .secret file
+                                   						    touch .secret
+                            sleep 1
+							echo excellent!!!
+                            echo provide read write permissions to .secret file
+                                                            chmod 600 ~/thesecret/src/secretDir/.secret
+                            echo Permissions granted
+							echo leaving src folder
+															cd ..
+ 		                    sleep 1
             #Dealing with malicious & links
-                                                    echo
-						    rm -r maliciousFiles
-                                                    echo
-						    find -xtype l -delete
-  #Final - Execute bash script
-                                                    echo
-						    /bin/bash generateSecret.sh
-                             echo dont give up now you almost there.....
-                             sleep 1
-		#revele the damn secret	     
-		                       		    echo
-						    cat secretDir/.secret  
+                            echo OH NO... Someone trying to hack me...
+							echo Deleting suspicious content
+                                   						    rm -r maliciousFiles
+                            echo Unlink the junk
+                                   						    find -xtype l -delete
+ #Execute bash script
+                            echo GENERATING MY SECRET - KEEP IT BETWEEN US
+                                   						    /bin/bash generateSecret.sh
+                            echo dont give up now you almost there.....
+                            sleep 1
 
-###RoyOps magic script
-       
+ #delete unnecessary content from src folder
+                            echo clean up your mess
+                            echo deleting files
+                                                            rm -r CONTENT_TO_HASH
+                                                            rm -r generateSecret.sh
+                                                            rm -r yourSolution.sh
 
-
-
-
-
+ 	        #revele the damn secret
+                            echo MY SECRET IS:
+ 	                                   					    cat secretDir/.secret
+                            echo Thank you for flying -=RoyOps=-
