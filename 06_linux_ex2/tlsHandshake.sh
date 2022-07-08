1311d31157199c3ff58edc3e088eca48a7d3eaf1
saaremez <saaremez@gmail.com>
2022-07-09 00:59:41 +0300

    8.7

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,2 +1,4 @@
-curl -X POST  http://16.16.53.16:8080/clienthello -H "Content-Type: application/json" -d '{ "clientVersion": "3.2", "message": "Client Hello"}'
-SESSION_ID=3e1ed23d-f437-4ebc-9c28-965c926a022
+curl -X POST  http://16.16.53.16:8080/clienthello -H "Content-Type: application/json" -d '{ "clientVersion": "3.2", "message": "Client Hello"}' > A
+cat A | jq -r '.sessionID'
+SESSION_ID=$(!!)
+echo $SESSION_ID
