89ea85451e8b5e6472c5db7ce60073ea18678b98
saaremez <saaremez@gmail.com>
2022-07-08 23:03:08 +0300

    8.7

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,1 +1,1 @@
-curl -X POST http://16.16.53.16 -H "Content-Type: application/json" -d "{ \"clientVersion\": \"3.2\", \"message\": \"Client Hello\" }"
+curl -X POST  http://16.16.53.16:8080/clienthello -H "Content-Type: application/json" -d '{ "clientVersion": "3.2", "message": "Client Hello"}'
