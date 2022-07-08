521cc9f296086235be127f6dac03292ad6ff5f90
naor <naorbenarii@gmail.com>
2022-07-08 10:52:57 +0300

    update

--- 01_linux_ex1/yourSolution.sh
+++ 01_linux_ex1/yourSolution.sh
@@ -1,21 +1,17 @@
 tar -zxvf secretGenerator.tar.gz
 #goes to check if any  maliciousFiles are existing
-
 cd src/
 chmod +x generateSecret.sh
-
+#need to make a directory for secret file
 mkdir secretDir
-
 touch secretDir/.secret
-
-chmod u+rw secretDir/.secret
-
+#creates a file . secret
+chmod 764 secretDir/.secret
+#gives the file permissions
 chmod go-rw secretDir/.secret
 
 rm maliciousFiles/*
 
 rmdir maliciousFiles/
-
 rm important.link
-
 ./generateSecret.sh
