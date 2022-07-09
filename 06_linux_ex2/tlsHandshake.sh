d3150ae20fac5164df67bd2cd3b2ad15539817ec
saaremez <saaremez@gmail.com>
2022-07-09 03:03:59 +0300

    8.7

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,11 +1,9 @@
 curl -X POST  http://16.16.53.16:8080/clienthello -H "Content-Type: application/json" -d '{ "clientVersion": "3.2", "message": "Client Hello"}' > A
 cat A | jq -r '.sessionID'
 SESSION_ID=$(!!)
-echo $SESSION_ID
 cat A | jq -r '.serverCert' > cert.pem
 openssl verify -CAfile cert-ca-aws.pem cert.pem
 VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
-
 if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid."
   exit 1
