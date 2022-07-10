db713b787382058927f263255473989b34393d2f
shimi bashiri <shimibas@gmail.com>
2022-07-10 11:41:37 +0300

    home work linux ex1

---
+++ 01_linux_ex1/yourSolution.sh
@@ -0,0 +1,27 @@
+## secret generated without any errors.
+
+wget https://devops-may22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
+shimibas@Strix:~$ ll
+tar -xf secretGenerator.tar.gz
+cd src
+ll
+ /bin/bash generateSecret.sh
+ mkdir secretDir
+ ll
+ /bin/bash generateSecret.sh
+ rm maliciousFiles/
+ rm -rf maliciousFiles/
+ ll
+ /bin/bash generateSecret.sh
+ cd secretDir/
+ touch .secret
+ chmod 600 .secret
+ cd ..
+ ll
+ rm important.link
+ /bin/bash generateSecret.sh
+  cd secretDir/
+  cat .secret
+
+  done
+
