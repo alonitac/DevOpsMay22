d1c9ebaa9fc0e54d19a239d7b1581c19417e84b2
RoyOps <104723013+RoyOps@users.noreply.github.com>
2022-07-11 13:00:53 +0300

    wip

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -5,5 +5,7 @@
 #curl -i -X POST -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
 #sessionID=$session_ID
 
+
+sessionID=$session_ID
 curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
 
