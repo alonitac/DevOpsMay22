e5f6b6d1595755e86b1215a9819588cca4141e5f
RoyOps <104723013+RoyOps@users.noreply.github.com>
2022-07-11 11:21:34 +0300

    wip

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://localhost:8080/update-profile
+curl -i -X POST -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello
