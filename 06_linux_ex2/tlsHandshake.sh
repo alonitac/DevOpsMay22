8add993cebade31ed6017adeed4fff621d580e0b
RoyOps <104723013+RoyOps@users.noreply.github.com>
2022-07-11 16:16:44 +0300

    wip 11072022 1615

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -15,14 +15,13 @@ openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | ba
 MASTER_KEY=$(tail  masterkey.txt)
 curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.17:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 
-sessionID=SESSION_ID
-masterKey.txt=$MASTER_KEY
-curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.sessionID' > sessionID
-curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
-wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
-openssl verify -CAfile cert-ca-aws.pem cert.pem
-openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0 > $MASTER_KEY
-curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"
-}' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+#sessionID=SESSION_ID
+#masterKey.txt=$MASTER_KEY
+#curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.sessionID' > sessionID
+#curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
+#wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+#openssl verify -CAfile cert-ca-aws.pem cert.pem
+#openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0 > $MASTER_KEY
+#curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 
 
