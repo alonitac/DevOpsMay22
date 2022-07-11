5e13e45ed1ffcfc1c5e10502b28c26e5e96223fb
RoyOps <104723013+RoyOps@users.noreply.github.com>
2022-07-11 14:01:23 +0300

    wip

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -10,6 +10,7 @@ sessionID=$session_ID
 curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert.,sessionID' > cert.pem
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
 openssl verify -CAfile cert-ca-aws.pem cert.pem
+touch masterKey.txt
 openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0
 
 
