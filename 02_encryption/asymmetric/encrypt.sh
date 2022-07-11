d1f0c646b865f03212f0320904d75620364526fc
naor <naorbenarii@gmail.com>
2022-07-11 03:33:05 +0300

    update

--- 02_encryption/asymmetric/encrypt.sh
+++ 02_encryption/asymmetric/encrypt.sh
@@ -1,1 +1,1 @@
-echo 'Hi Alice! Please send 500$ to Bob' | openssl rsautl -encrypt -pubin -inkey ~/.ssh/id_rsa.pub.pem > message.encrypted
+echo 'Hi Alice! Please send 500$ to Bob' | openssl rsautl -encrypt -pubin -inkey ~/.ssh/id_rsa.pub > message.encrypted
