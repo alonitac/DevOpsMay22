9bd40295d4f25231bb6f84a31ef7a7a0dca412c0
RoyOps <104723013+RoyOps@users.noreply.github.com>
2022-07-11 16:24:16 +0300

    test wip

---
+++ 06_linux_ex2/handshaketest.sh
@@ -0,0 +1,18 @@
+curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.sessionID' > sessionID.txt
+
+SESSION_ID=$(tail -n 1 sessionID.txt)
+
+curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
+
+wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+
+openssl verify -CAfile cert-ca-aws.pem cert.pem
+
+touch masterkey.txt
+
+openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | base64 -w 0 > masterkey.txt
+
+MASTER_KEY=$(tail  masterkey.txt)
+
+curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+
