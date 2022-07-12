577735bcef4db97e01172fd6b24b30d079b1bb05
RoyOps <104723013+RoyOps@users.noreply.github.com>
2022-07-12 15:29:37 +0300

    test tlsHandshake sh script v1.1

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,8 +1,8 @@
-curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.sessionID' > sessionID.txt
+curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://127.0.0.1:8080/clienthello | jq -r '.sessionID' > sessionID.txt
 
 SESSION_ID=$(tail sessionID.txt)
 
-curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
+curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://127.0.0.1:8080/clienthello | jq -r '.serverCert' > cert.pem
 
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
 
@@ -18,11 +18,15 @@ openssl rand -base64 32 > masterkey.txt
 
 MASTER_KEY=$( openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | base64 -w 0 )
 
-curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://127.0.0.1:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 
 cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
 
+rm -r encSampleMsg.txt
+
 openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterkey.txt -in encSampleMsgReady.txt -out decSampleMsgReady.txt
+
+rm -r encSampleMsgReady.txt
 
 DECRYPTED_SAMPLE_MESSAGE=$(tail decSampleMsgReady.txt)
 
