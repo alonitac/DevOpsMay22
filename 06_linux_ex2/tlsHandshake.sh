97ff8878e0b354ca37f8e9b7db4843ca05fe37f9
RoyOps <104723013+RoyOps@users.noreply.github.com>
2022-07-12 14:24:02 +0300

    Final tls handshake sh script

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,27 +1,35 @@
-#curl -i -X POST -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq '.sessionID,.serverCert' > info.txt
-#curl -s --header "Content-Type: application/json" -d "{\"clientVersion\":\"3.2\",\"message\":\"Client Hello\"}" http://16.16.53.16:8080/clienthello | jq -r '.serverCert,.sessionID' > info.txt
-#curl -i -X POST -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq '.sessionID,.serverCert' > info.txt
-
-#curl -i -X POST -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
-#sessionID=$session_ID
-
 curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.sessionID' > sessionID.txt
+
 SESSION_ID=$(tail -n 1 sessionID.txt)
+
 curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
+
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+
 openssl verify -CAfile cert-ca-aws.pem cert.pem
-touch masterkey.txt
-openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | base64 -w 0 > masterkey.txt
-MASTER_KEY=$(tail  masterkey.txt)
-curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.17:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 
-#sessionID=SESSION_ID
-#masterKey.txt=$MASTER_KEY
-#curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.sessionID' > sessionID
-#curl --header "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
-#wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
-#openssl verify -CAfile cert-ca-aws.pem cert.pem
-#openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0 > $MASTER_KEY
-#curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
 
+if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
+  echo "Server Certificate is invalid."
+  exit 1
+fi
 
+openssl rand -base64 32 > masterkey.txt
+
+MASTER_KEY=$( openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | base64 -w 0 )
+
+curl -s --header "Content-Type: application/json" -d '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+
+cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
+
+openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterkey.txt -in encSampleMsgReady.txt -out decSampleMsgReady.txt
+
+DECRYPTED_SAMPLE_MESSAGE=$(tail decSampleMsgReady.txt)
+
+if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
+  echo "Server symmetric encryption using the exchanged master-key has failed."
+  exit 1
+else
+  echo "Client-Server TLS handshake has been completed successfully"
+fi
