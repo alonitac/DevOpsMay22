718fed6c0eaadcc20b33de5319af469c4c76b744
polonskyilya <ilya@polonsky.net>
2022-07-15 21:08:11 +0300

    Process states

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 correctMessage=$(echo('Hi server, please encrypt me and send to client!'))
 curl -# -o 'response.json' -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' -X POST http://16.16.53.16:8080/clienthello
 sessionId=$(jq -r '.sessionID' response.json)
