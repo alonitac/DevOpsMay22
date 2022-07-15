02e7317689aea49eec21420739e94ea0c2fe9839
polonskyilya <ilya@polonsky.net>
2022-07-15 23:28:47 +0300

    Fixes after workflow test fail

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-correctMessage=$(echo('Hi server, please encrypt me and send to client!'))
 curl -# -o 'response.json' -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' -X POST http://16.16.53.16:8080/clienthello
 sessionId=$(jq -r '.sessionID' response.json)
 jq -r '.serverCert' response.json>cert.pem
@@ -9,13 +8,16 @@ if [ "$verificationResult" != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid."
   exit 1
 fi
-openssl rand -base64 32 >masterkey.txt
-openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | base64 -w 0
-curl -# -o 'response_message.json' -H "Content-Type: application/json" -d '{"sessionID": "'$sessionId'","masterKey": "'$masterKey'","sampleMessage": "'$correctMessage'"}' -X POST http://16.16.53.16:8080/keyexchange
+openssl rand -out masterkey.txt -base64 32
+masterKey=$(cat masterkey.txt)
+masterKey=$(openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | base64 -w 0)
+curl -# -o 'response_message.json' -H "Content-Type: application/json" -d '{"sessionID": "'$sessionId'","masterKey": "'$masterKey'","sampleMessage": "Hi server, please encrypt me and send to client!"}' -X POST http://16.16.53.16:8080/k>
 jq -r '.encryptedSampleMessage' response_message.json | base64 -d > encSampleMsgReady.txt
 decryptedSampleMessage=$(openssl enc -d -aes-256-cbc -pbkdf2 -kfile masterkey.txt -in encSampleMsgReady.txt)
-if [ "$decryptedSampleMessage" != $correctMessage ]; then
+echo $decryptedSampleMessage
+if [ "$decryptedSampleMessage" != 'Hi server, please encrypt me and send to client!' ]; then
   echo "Server symmetric encryption using the exchanged master-key has failed."
   exit 1
 else
   echo "Client-Server TLS handshake has been completed successfully"
+fi
