53dad38f0ca81e15f1fa1b35d798b18d4a22fc78
saaremez <saaremez@gmail.com>
2022-07-15 19:26:47 +0300

    15.7

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -X POST  http://16.16.53.16:8080/clienthello -H "Content-Type: application/json" -d '{ "clientVersion": "3.2", "message": "Client Hello"}' > A
 SESSION_ID=$(cat A | jq -r '.sessionID')
 cat A | jq -r '.serverCert' > cert.pem
