41ed19c23a92f80b1bd9c5cc2424567b74875993
Vladislav Spector <107487272+vladislav-spector@users.noreply.github.com>
2022-07-16 21:56:22 +0300

    [WIP] Linux ex2 solution - change 2

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-
-curl -X POST -d '{"clientVersion": "3.2", "message": "Client Hello"}' -H 'Content-Type: application/json' http://16.16.53.16:8080/clienthello | jq -r '.serverVersion' > serverVersion
-curl -X POST -d '{"clientVersion": "3.2", "message": "Client Hello"}' -H 'Content-Type: application/json' http://16.16.53.16:8080/clienthello | jq -r '.sessionID' > sessionID
-curl -X POST -d '{"clientVersion": "3.2", "message": "Client Hello"}' -H 'Content-Type: application/json' http://16.16.53.16:8080/clienthello | jq -r '.serverCert' > cert.pem
-
+curl -# -o 'response.json' -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' -X POST http://16.16.53.16:8080/clienthello
+sessionId=$(jq -r '.sessionID' response.json)
+sampleMessage='Hi server, please encrypt me and send to client!'
+jq -r '.serverCert' response.json>cert.pem
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
-openssl verify -CAfile cert-ca-aws.pem cert.pem
-
-openssl rand -out masterKey.txt -base64 32
-openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0 > MASTER_KEY
-
-SESSION_ID=$(<sessionID)
-MASTER_KEY=$(<MASTER_KEY)
-curl -X POST -d '{"sessionID":"'$SESSION_ID'","masterKey":"'$MASTER_KEY'","sampleMessage":"Hi server, please encrypt me and send to client!"}' -H 'Content-Type: application/json' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
-
-cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
-
-masterKey=$(<masterKey.txt)
-openssl enc -d -aes-256-cbc -pbkdf2 -k $masterKey -in encSampleMsgReady.txt -out DECRYPTED_SAMPLE_MESSAGE
-
-DECRYPTED_SAMPLE_MESSAGE=$(<DECRYPTED_SAMPLE_MESSAGE)
-
-if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
+verificationResult=$(openssl verify -CAfile cert-ca-aws.pem cert.pem)
+if [ "$verificationResult" != "cert.pem: OK" ]; then
+  echo "Server Certificate is invalid."
+  exit 1
+fi
+openssl rand -out masterkey.txt -base64 32
+masterKey=$(openssl smime -encrypt -aes-256-cbc -in masterkey.txt -outform DER cert.pem | base64 -w 0)
+curl -# -o 'response_message.json' -H "Content-Type: application/json" -d '{"sessionID": "'$sessionId'","masterKey": "'$masterKey'","sampleMessage": "Hi server, please encrypt me and send to client!"}' -X POST http://16.16.53.16:8080/keyexchange
+jq -r '.encryptedSampleMessage' response_message.json | base64 -d > encSampleMsgReady.txt
+decryptedSampleMessage=$(openssl enc -d -aes-256-cbc -pbkdf2 -kfile masterkey.txt -in encSampleMsgReady.txt)
+echo 'Start Encrypted Message-'
+echo $(cat encSampleMsgReady.txt)
+echo 'End Encrypted Message---'
+echo '=============================='
+echo 'Start Decrypted Message-'
+echo $decryptedSampleMessage
+echo 'End Decrypted Message---'
+if [ "$decryptedSampleMessage" != "Hi server, please encrypt me and send to client!" ]; then
   echo "Server symmetric encryption using the exchanged master-key has failed."
   exit 1
 else
