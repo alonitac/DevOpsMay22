9224e1e5ab7d645b02350f3d479bef4c4395af9d
uri <urits91@gmail.com>
2022-07-21 20:02:24 +0300

    #decrypting...

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -5,10 +5,13 @@ echo "Connecting to the server"
 SERVER=$(curl -s -X POST -H "Content-Type: application/json" -d '{"clientVersion": "3.2","message": "Client Hello"}' http://16.16.53.16:8080/clienthello)
 
 SESSION_ID=$(echo $SERVER | jq -r '.sessionID')
+
 echo $SERVER | jq -r '.serverCert' > cert.pem
 echo "Downloadning files...."
 sleep 5
+
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+
 VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
 echo "Loading....."
 sleep 5
@@ -22,11 +25,17 @@ if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
 fi
 
 openssl rand -base64 32 > masterKey.txt
+
 MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
+
 curl -s -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.16:8080/keyexchange |jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+
 cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
+
 ENCRYPTION_PASSWORD=$(cat masterKey.txt)
+
 openssl enc -d -aes-256-cbc -pbkdf2 -k "$ENCRYPTION_PASSWORD" -in encSampleMsgReady.txt -out decrypt.txt
+
 DECRYPTED_SAMPLE_MESSAGE=$(cat decrypt.txt )
 
 if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
