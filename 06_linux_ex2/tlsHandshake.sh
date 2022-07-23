f8bf8b1636fc240b0bd9566c8a4828010737e2ef
eitan <eitanh@malam-gemel.com>
2022-07-23 18:34:01 +0300

    tls answer

---
+++ 06_linux_ex2/tlsHandshake.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+curl -X POST -H "Content-Type: application/json"  -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://16.16.53.16:8080/clienthello | jq -r  >temp_reg.txt
+SESSION_ID=$(cat temp_reg.txt | jq -r '.sessionID')
+cat temp_reg.txt | jq -r '.serverCert' >cert.pem
+
+wget  https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+
+VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
+
+if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
+  echo "Server Certificate is invalid."
+  exit 1
+fi
+
+openssl rand -base64 32 >masterKey.txt
+
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
+
+curl -X POST -H "Content-Type: application/json"  -d '{"sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' http://16.16.53.16:8080/keyexchange | jq -r '.encryptedSampleMessage' >encSampleMsg.txt
+
+cat encSampleMsg.txt  | base64 -d > encSampleMsgReady.txt
+
+openssl enc -d -aes-256-cbc -pbkdf2 -kfile masterKey.txt -in encSampleMsgReady.txt -out decSampleMsgReady.txt
+DECRYPTED_SAMPLE_MESSAGE=$(cat decSampleMsgReady.txt)
+
+if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
+  echo "Server symmetric encryption using the exchanged master-key has failed."
+  exit 1
+else
+  echo "Client-Server TLS handshake has been completed successfully"
+fi
