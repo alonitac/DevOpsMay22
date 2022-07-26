b1180d6d639894893692afa6f3df9b80069a92a5
יובל עינת <einatman4.5@gmail.com>
2022-07-26 21:32:05 +0300

    fixing

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -5,10 +5,9 @@ curl -X POST http://16.16.53.16:8080/clienthello \
 --data '{"clientVersion":"3.2","message":"Client Hello"}' | \
 jq -r '.serverCert,.sessionID' > combined.txt
 sed -n '1,34 p' temp.txt > cert.pem
-SESSION-ID=$(tail -n 1 combined.txt)
+SESSION_ID=$(tail -n 1 combined.txt)
 wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
-sleep 10
-VERIFICATION_RESULT=$(openssl verify -CAfile cert-ca-aws.pem cert.pem)
+VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
 
 if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid."
@@ -19,7 +18,7 @@ openssl rand -out masterKey.txt -base64 32
 ENCRYPTED_MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
 curl -X POST http://16.16.53.16:8080/keyexchange \
 -H "Content-Type: application/json" \
---data '{"sessionID": "$SESSION-ID", "masterKey": "$ENCRYPTED_MASTER_KEY", "sampleMessage": "Hi server, please encrypt me and send to client!"}' | \
+--data '{"sessionID": "'"$SESSION_ID"'", "masterKey": "'"$ENCRYPTED_MASTER_KEY"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' | \
 jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
 DECRYPTED_SAMPLE_MESSAGE=$(openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterKey.txt -in encSampleMsgReady.txt -out encFinalmessage.txt)
