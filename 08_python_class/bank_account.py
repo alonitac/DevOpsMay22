4ecd5a04ac2914925236596aa3db49a12fa6f183
alonit <alon.itach@mail.huji.ac.il>
2022-08-18 20:30:50 +0300

    bank account demo - wip

--- 08_python_class/bank_account.py
+++ 08_python_class/bank_account.py
@@ -16,14 +16,36 @@ Instantiate your class, make several deposits and withdrawals, and test to make 
 
 class Account:
     def __init__(self, name, balance):
-        pass  # your implementation here!
+        self.balance = balance
+        self.owner = name
+
+    def __len__(self):
+        return self.balance
+
+    def __str__(self):
+        return f'Account owner:   {self.owner}\nAccount balance: ${self.balance}'
+
+    def __add__(self, other):
+        """
+        :param other: of type Account!!
+        :return:
+        """
+        new = Account(self.owner + ' and ' + other.owner, self.balance + other.balance)
+        self.balance = 0
+        other.balance = 0
+        return new
 
 
 if __name__ == '__main__':
 
     # 1. Instantiate the class
     acct1 = Account('Jose', 100)
+    acct2 = Account('Jane', 1000)
 
+    acct3 = acct1 + acct2  # acct1.__add__(acct2)
+
+    # print(len(acct1))
+    x = str(acct1)
 
     # 2. Print the object
     print(acct1)
