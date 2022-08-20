09b3e2b16e9befde068f7755d7db90715a5ee8b8
Vladislav Spector <107487272+vladislav-spector@users.noreply.github.com>
2022-08-20 18:54:47 +0300

    Merge remote-tracking branch 'origin/linux_ex2/vladislav-spector' into linux_ex2/vladislav-spector

---
+++ python_katas/kata_1/test.py
@@ -0,0 +1,183 @@
+import unittest
+from python_katas.kata_1 import questions
+from python_katas.utils import unittest_runner
+
+
+class TestSumOfElements(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_empty_list(self):
+        lst = []
+        self.assertEqual(questions.sum_of_element(lst), 0)
+
+    def test_integers_list(self):
+        lst = [1, 2, 3, 4, 5]
+        self.assertEqual(questions.sum_of_element(lst), 15)
+
+    def test_negative_numbers(self):
+        lst = [1, -6, 7, 0, 99]
+        self.assertEqual(questions.sum_of_element(lst), 101)
+
+    def test_all_zeros(self):
+        lst = [0] * 50000
+        self.assertEqual(questions.sum_of_element(lst), 0)
+
+
+class TestWordsConcatenation(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_empty_list(self):
+        lst = []
+        self.assertEqual(questions.words_concatenation(lst), "")
+
+    def test_full_list(self):
+        lst = ['no', 'place', 'like', 'home']
+        self.assertEqual(questions.words_concatenation(lst), 'no place like home')
+
+    def test_empty_string_in_list(self):
+        lst = ['no', '', 'place', '', 'like', 'home']
+        self.assertEqual(questions.words_concatenation(lst), 'no place like home')
+
+    def test_list_of_empty_strings(self):
+        lst = ["","","","",""]
+        self.assertEqual(questions.words_concatenation(lst),"")
+
+
+class TestReverseWordsConcatenation(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestIsUniqueString(unittest.TestCase):
+    """
+    2 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestListDiff(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPrimeNumber(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPalindromeNum(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPairMatch(unittest.TestCase):
+    """
+    3 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestBadAverage(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestBestStudent(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPrintDictAsTable(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestMergeDicts(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestSevenBoom(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestCaesarCipher(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestSumOfDigits(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+if __name__ == '__main__':
+    import inspect
+    import sys
+    unittest_runner(inspect.getmembers(sys.modules[__name__], inspect.isclass))
