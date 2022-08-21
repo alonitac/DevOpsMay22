d1cdc23aedfc15f92185fe799a5c637a168e67b7
naorx <naorbenarii@gmail.com>
2022-08-22 00:27:46 +0300

    update

--- 08_python_class/bank_account.py
+++ 08_python_class/bank_account.py
@@ -15,34 +15,30 @@ Instantiate your class, make several deposits and withdrawals, and test to make 
 
 
 class Account:
-    def __init__(self, name, balance):
+
+    def __init__(self, owner, balance):
+        self.owner = owner
         self.balance = balance
-        self.owner = name
 
-    def __len__(self):
-        return self.balance
+    def deposit(self, balance):
+        self.balance += balance
+        print(f'Deposit accepted your current Balance is: {self.balance}$')
 
-    def __str__(self):
-        return f'Account owner:   {self.owner}\nAccount balance: ${self.balance}'
+    def withdraw(self, subtract):
+        self.balance -= subtract
+        if self.balance >= subtract:
+            print(f" Withdrawal Accepted  your current balance is: {self.balance}$")
 
-    def __add__(self, other):
-        """
-        :param other: of type Account!!
-        :return:
-        """
-        new = Account(self.owner + ' and ' + other.owner, self.balance + other.balance)
-        self.balance = 0
-        other.balance = 0
-        return new
+        elif self.balance <= subtract:
+            print(f" Funds Unavailable you're missing {self.balance}$")
 
 
 if __name__ == '__main__':
-
     # 1. Instantiate the class
     acct1 = Account('Jose', 100)
     acct2 = Account('Jane', 1000)
 
-    acct3 = acct1 + acct2  # acct1.__add__(acct2)
+    # acct3 = acct1 + acct2  # acct1.__add__(acct2)
 
     # print(len(acct1))
     x = str(acct1)
@@ -65,11 +61,9 @@ if __name__ == '__main__':
     acct1.deposit(50)
     # >> Deposit Accepted
 
-    acct1.withdraw(75)
+    acct1.withdraw(120)
     # Withdrawal Accepted
-
+    print()
     # 6. Make a withdrawal that exceeds the available balance
     acct1.withdraw(500)
     # Funds Unavailable!
-
-
