cd816a96d950ba9e8257c9431f7fd754f773677e
denizaychenko <denis.zaychenko@philips.com>
2022-08-25 17:38:04 +0300

    my commit

--- 08_python_class/bank_account.py
+++ 08_python_class/bank_account.py
@@ -16,17 +16,42 @@ Instantiate your class, make several deposits and withdrawals, and test to make 
 
 class Account:
     def __init__(self, name, balance):
-        pass
+        self.owner = name
+        self.balance = balance
 
+    def deposit(self, deposit):
+        self.balance += deposit
+        print (">> Deposit Accepted. New balance is : " + str(self.balance))
+
+    def withdraw(self, withdraw):
+        if withdraw > self.balance:
+            print ("Funds Unavailable!")
+        else :
+            self.balance -= withdraw
+            print (">> Withdrawal Accepted. New balance is : " + str(self.balance))
+
+    def __str__ (self) :
+        return f">> Account owner: {self.owner} \n>> Account balance: {self.balance}"
+
+    def __add__(self , other):
+        new =  Account(self.owner + ' and ' + other.owner ,self.balance + other.balance)
+        self.balance = 0
+        other.balance = 0
+        return new
 
 if __name__ == '__main__':
-
     # 1. Instantiate the class
     acct1 = Account('Jose', 100)
-
+    acct2 = Account('Moshe',200)
+    #acct3 = Account('Kuky', 4000)
 
     # 2. Print the object
     print(acct1)
+    print(acct2)
+    acct3 = acct1+acct2
+    print(acct3)
+    print(acct1)
+    print(acct2)
     # output:
     # >> Account owner:   Jose
     # >> Account balance: $100
@@ -49,5 +74,3 @@ if __name__ == '__main__':
     # 6. Make a withdrawal that exceeds the available balance
     acct1.withdraw(500)
     # Funds Unavailable!
-
-
