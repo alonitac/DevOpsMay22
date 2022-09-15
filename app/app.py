6515d34f6fe62dfec547630bfbbc07e4c0c15a83
alonit <alon.itach@mail.huji.ac.il>
2022-09-15 17:03:40 +0300

    docker tutorials - multi app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, request, render_template
 from yt_dlp import YoutubeDL
 from flask import jsonify
-import sqlite3
+import mysql.connector
+import os
 
-con = sqlite3.connect("data/videos.db", check_same_thread=False)
+con = mysql.connector.connect(
+    host="mysql",
+    user="root",
+    password=os.environ['DB_ROOT_PASS'],
+    database=os.environ['DB_NAME']
+)
+
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS videos(client_ip, title, videoId, UNIQUE(client_ip, title, videoId))")
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS videos(
+        client_ip varchar(255),
+        title varchar(255),
+        videoId varchar(255),
+        CONSTRAINT UC_video UNIQUE (client_ip, title, videoId))
+    """)
 
 app = Flask(__name__, static_url_path='')
 
@@ -21,8 +34,8 @@ def get_youtube():
     video_name = request.json['text']
 
     if video_name == 'my videos':
-        res = cur.execute(f"SELECT * FROM videos WHERE client_ip='{client_ip}'")
-        data = res.fetchall()
+        cur.execute(f"SELECT * FROM videos WHERE client_ip='{client_ip}'")
+        data = cur.fetchall()
         return jsonify({
             'items': [{'text': t, 'videoId': i} for _, t, i in data]
         }), 200
@@ -31,7 +44,7 @@ def get_youtube():
         videos = ydl.extract_info(f"ytsearch{1}:{video_name}", download=True)['entries']
 
         if videos:
-            cur.execute(f"""INSERT OR IGNORE INTO videos VALUES ('{client_ip}', '{videos[0]['title']}', '{videos[0]['id']}')""")
+            cur.execute(f"""INSERT IGNORE INTO videos VALUES ('{client_ip}', '{videos[0]['title']}', '{videos[0]['id']}')""")
             con.commit()
 
         return jsonify({
