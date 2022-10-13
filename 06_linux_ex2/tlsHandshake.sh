ff0632b5680bc9d3960a6943236f20a88a151724
alonit <alon.itach@mail.huji.ac.il>
2022-10-13 11:55:19 +0300

    linux ex2 feedback

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,3 +1,5 @@
+# TODO good job, clean and organized code
+
 curl -# -o 'response.json' -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' -X POST http://16.16.53.16:8080/clienthello
 sessionId=$(jq -r '.sessionID' response.json)
 jq -r '.serverCert' response.json>cert.pem
