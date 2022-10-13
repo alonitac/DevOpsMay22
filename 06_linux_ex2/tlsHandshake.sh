073d0d03f6a8fe9180126643a83d838770e03540
alonit <alon.itach@mail.huji.ac.il>
2022-10-13 11:45:06 +0300

    linux ex2 feedback

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+# TODO good job Mustafa
+
 curl -# -o 'response.json' -H "Content-Type: application/json" -d '{"clientVersion": "3.2", "message": "Client Hello"}' -X POST http://16.16.53.16:8080/clienthello
 sessionId=$(jq -r '.sessionID' response.json)
 sampleMessage='Hi server, please encrypt me and send to client!'
